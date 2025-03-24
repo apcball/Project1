f8028fc3bf4ac1c2028379106bbff609cb4c2070
apcball <apichart.apcball@gmail.com>
2025-03-24 16:47:45 +0700

    OK

--- import_bom_new.py
+++ import_bom_new.py
@@ -13,7 +13,7 @@ def read_excel_template():
     """Read and validate the Excel template"""
     try:
         # Read the Excel file
-        df = pd.read_excel('Data_file/import_bom_bu2_บันใด.xlsx')
+        df = pd.read_excel('Data_file/import_bom_2.xlsx')
         
         # Clean up the data
         df = df.fillna('')  # Replace NaN with empty string
