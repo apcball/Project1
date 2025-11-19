2598c56f429ddc2aa5b6b532797fa0cb4c0a1479
Apichart <apichart.apcball@gmail.com>
2025-11-19 14:07:51 +0700

    ok

--- Import_Product/import_product_new.py
+++ Import_Product/import_product_new.py
@@ -336,7 +336,7 @@ def process_image(image_path, product_hint=None):
 failed_imports = []
 
 # --- อ่านข้อมูลจากไฟล์ Excel ---
-excel_file = 'Import_Product/import_product_expens.xlsx'
+excel_file = 'Import_Product/import_product_New1.xlsx'
 try:
     df = pd.read_excel(excel_file)
     print(f"Excel file '{excel_file}' read successfully. Number of rows = {len(df)}")
